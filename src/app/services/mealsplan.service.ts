097212af6d75f326b354f4ca1049f84e7cc3194d
David Koehn <davidnkoehn@yahoo.com>
2025-06-30 12:50:49 -0500

    made all of the functions to get the meals and make and update meals

--- src/app/services/mealsplan.service.ts
+++ src/app/services/mealsplan.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Meal } from '../models/meal';
+import { MPlan } from '../models/m-plan';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MealsplanService {
 
-  constructor() { }
+baseUrl = 'http://localhost:5251/api/meals'
+
+  constructor(private http: HttpClient) { }
+
+  GetAllMealsPlanByUser(Id: number): Observable<any> {
+    return this.http.get<{meals:any }>(`${this.baseUrl}${Id}`);
+  }
+
+  CreateMealPlan(meal: MPlan){
+    return this.http.post(`${this.baseUrl}/create`, meal);
+  }
+
+  UpdateMealPlanById(meal: MPlan){
+    return this.http.put(`${this.baseUrl}/update${meal.mealId}`, meal);
+  }
+
+  DeleteMealPlanByMealsId(id:number){
+    return this.http.delete(`${this.baseUrl}/delete${id}`);
+  }
+
+  GetMealPlanByMealId(id:number){
+    return this.http.get(`${this.baseUrl}/mealId${id}`);
+  }
 }
